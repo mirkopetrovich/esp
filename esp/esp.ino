8823430645c7f8348bd88b6ec82149f147c91dc6
mirko <mirko.petrovich@gmail.com>
2024-01-11 18:22:52 -0300

    pines arduino

--- esp/esp.ino
+++ esp/esp.ino
@@ -2,10 +2,21 @@ unsigned long tiempo;
 int segundos,minutos,horas;
 int ss,mm ,hh;
 int militar;
-int preset;
-int multiplicador = 10;
+int preset = -1;
+int multiplicador = 1;
+
+// MOTOR 2
+
+
+
+
 
 void setup() {
+
+  pinMode(23, OUTPUT);
+  pinMode(25, OUTPUT);
+  pinMode(7,OUTPUT);
+  digitalWrite(7, LOW);
 
  Serial.begin(9600);
 
@@ -25,15 +36,33 @@ void loop() {
  ss = segundos%60;
  militar = ss+(mm*100)+(hh*10000);
 
- if ((militar>=100)&&(militar<120)) {
-  if (preset!=1) {
-   preset_1();
+  if ((militar>=5)&&(militar<6)) {
+  if (preset!=0) {
+   preset_0(); //  ATOMIZADORES
   }
  }
 
-  if ((militar>=150)&&(militar<160)) {
+ if ((militar>=10)&&(militar<11)) {
+  if (preset!=1) {
+   preset_1(); //  abre
+  }
+ }
+
+  if ((militar>=12)&&(militar<13)) {
   if (preset!=2) {
-   preset_2();
+   preset_2(); // apaga
+  }
+ }
+ 
+   if ((militar>=15)&&(militar<16)) {
+  if (preset!=3) {
+   preset_3(); // ciera
+  }
+ }
+ 
+   if ((militar>=17)&&(militar<18)) {
+  if (preset!=4) {
+   preset_4(); //apaga
   }
  }
  
@@ -47,15 +76,46 @@ void calibra() {
   delay (3000);
 }
 
+void preset_0() {
+  preset=0; 
+  Serial.println(" -----------------> ATOMIZADORES");
+  digitalWrite(7,HIGH); 
+  
+  
+}
+
+
 void preset_1() {
   preset=1; 
   Serial.println(" -----------------> PRESET 1");
+  digitalWrite(23,HIGH);  //abre
+  digitalWrite(25,LOW);
+  
 }
 
 void preset_2() {
   preset=2; 
   Serial.println(" -----------------> PRESET 2");
+ digitalWrite(23,HIGH);  
+  digitalWrite(25, HIGH);
 }
+
+void preset_3() {
+  preset=3; 
+  Serial.println(" -----------------> PRESET 3");
+  digitalWrite(23,LOW);
+  digitalWrite(25, HIGH);
+}
+
+void preset_4() {
+  preset=4; 
+  Serial.println(" -----------------> PRESET 4");
+  digitalWrite(23,HIGH);
+  digitalWrite(25, HIGH);
+}
+
+
+
 
 void print() {
  if (hh<10) Serial.print("0");
@@ -70,6 +130,10 @@ void print() {
  Serial.println(militar);
  
 }
+
+
+
+
 
 
 
